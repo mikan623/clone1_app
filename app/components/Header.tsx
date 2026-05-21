313d8a523bfe7391e2fe8ab3182b98428431e802
原　紫乃花 <shinoka@suzukinoMacBook-Pro.local>
2026-05-21 14:24:51 +0900

    API表示ページを変更

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
         </div>
       </div>
       <header className="border-b border-gray-200">
-        <div className="mx-auto flex max-w-5xl items-center justify-between px-6 py-4">
+        <div className="mx-auto flex max-w-5xl items-center justify-between px-5 py-4">
           <Link href="/">
             <Image 
               src={logoImage} 
